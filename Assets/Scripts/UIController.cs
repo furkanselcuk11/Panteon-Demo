352774cfcabaa2b2eee8f78a0e3a672fdb1fb246
Furkan Selçuk <64227907+furkanselcuk11@users.noreply.github.com>
2022-07-28 13:20:46 +0300

    +add: TaptoStart and UIManager

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController uicontrollerInstance;
+
+    [Header("Game UI Controller")]
+    [SerializeField] private GameObject GameStartPanel;
+    [SerializeField] private GameObject GameRunTimePanel;
+    [SerializeField] private GameObject GameFinishPanel;
+    
+   
+    private void Awake()
+    {
+        if (uicontrollerInstance == null)
+        {
+            uicontrollerInstance = this;
+        }
+    }
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        UpdatePanel();
+    }
+    public void UpdatePanel()
+    {
+        if (GameManager.gamemanagerInstance.startGame & !GameManager.gamemanagerInstance.isFinish)
+        {
+            GameStartPanel.SetActive(false);
+            GameRunTimePanel.SetActive(true);
+            GameFinishPanel.SetActive(false);
+        }
+        else
+        {
+            GameStartPanel.SetActive(false);
+            GameRunTimePanel.SetActive(false);
+            GameFinishPanel.SetActive(true);
+        }
+        if (!GameManager.gamemanagerInstance.startGame & !GameManager.gamemanagerInstance.isFinish)
+        {
+            GameStartPanel.SetActive(true);
+            GameRunTimePanel.SetActive(false);
+            GameFinishPanel.SetActive(false);
+        }
+    }
+}
